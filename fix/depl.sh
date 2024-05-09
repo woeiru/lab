81ba33bd70fa9264c9ffa2c74206097cd4269d00
root <root@c18.fritz.box>
2024-05-09 23:22:25 +0200

    standard

--- fix/depl.sh
+++ fix/depl.sh
@@ -1,5 +1,17 @@
-sudo cp disable-devices-as-wakeup.service /etc/systemd/system/
-sudo cp disable-devices-as-wakeup.sh /etc/systemd/system/
+#!/bin/bash
 
+# Get the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Copy service file to systemd directory
+cp "$DIR/disable-devices-as-wakeup.service" /etc/systemd/system/
+
+# Copy script file to systemd directory
+cp "$DIR/disable-devices-as-wakeup.sh" /etc/systemd/system/
+
+# Enable and start the service
 systemctl enable disable-devices-as-wakeup.service
+systemctl start disable-devices-as-wakeup.service
+
+# Check the status of the service
 systemctl status disable-devices-as-wakeup.service
