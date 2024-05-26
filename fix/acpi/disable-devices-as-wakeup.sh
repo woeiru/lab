02c03cce9d97a8a874443efa321d494338aaaddf
woeiru <169383590+woeiru@users.noreply.github.com>
2024-05-27 00:15:33 +0200

    Enhance ACPI wake-up prevention script for better device handling

--- fix/acpi/disable-devices-as-wakeup.sh
+++ fix/acpi/disable-devices-as-wakeup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-declare -a devices=(GPP0) # <-- Add your entries here
+LOGFILE="/var/log/disable-devices-as-wakeup.log"
+
+echo "Script started at $(date)" >> $LOGFILE
+declare -a devices=("GPP0") # <-- Add your entries here
 for device in "${devices[@]}"; do
-    if grep -qw ^$device.*enabled /proc/acpi/wakeup; then
-        sudo sh -c "echo $device > /proc/acpi/wakeup"
+    if grep -qw ^$device /proc/acpi/wakeup; then
+        status=$(grep -w ^$device /proc/acpi/wakeup | awk '{print $3}')
+        echo "Device $device found with status $status" >> $LOGFILE
+        if [ "$status" == "*enabled" ]; then
+            echo $device > /proc/acpi/wakeup
+            echo "Device $device disabled" >> $LOGFILE
+        else
+            echo "Device $device was already disabled" >> $LOGFILE
+        fi
+    else
+        echo "Device $device not found" >> $LOGFILE
     fi
 done
+echo "Script finished at $(date)" >> $LOGFILE
 
