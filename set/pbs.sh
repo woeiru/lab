37e0ecd729c44880200f2933334ab2ef3a21a01d
woeiru <169383590+woeiru@users.noreply.github.com>
2024-06-09 19:35:52 +0200

    standard

--- set/pbs.sh
+++ set/pbs.sh
@@ -8,6 +8,29 @@ BASE="${FILE%.*}"
 # Source config.sh using the absolute path
 source "$DIR/../var/${BASE}.conf"
 
+# main setup function
+setup_main() {
+    if [ "$#" -eq 0 ]; then
+        setup_display_menu
+        setup_read_choice
+    else
+        setup_execute_arguments "$@"
+    fi
+}
+
+# main read choice
+setup_read_choice() {
+    read -p "Enter your choice: " choice
+    setup_execute_choice "$choice"
+}
+
+# main execute choice
+setup_execute_arguments() {
+    for arg in "$@"; do
+        setup_execute_choice "$arg"
+    done
+}
+
 # display setup_main menu
 setup_display_menu() {
     echo "Choose an option:"
