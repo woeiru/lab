a7f6a3f42e86e29b77d72e1fffa5cf9b493d3073
woeiru <169383590+woeiru@users.noreply.github.com>
2024-06-26 17:27:47 +0200

    standard

--- set/nfs.sh
+++ set/nfs.sh
@@ -1,47 +1,31 @@
 #!/bin/bash
 
-# sourcing
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-file=$(basename "${BASH_SOURCE[0]}")
-source "$dir/.up"
-setup_source "$dir" "$file"
+# Sourcing
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FILE=$(basename "${BASH_SOURCE[0]}")
+source "$DIR/.up"
+setup_source "$DIR" "$FILE"
 
-# call menu structure
+# Call menu structure
 setup_main "$@"
 
-# display setup_main menu
-setup_display_menu() {
-    echo "Choose an option:"
-    echo "a......................( include config )"
-    echo "a1. install packages"
-    echo "a2. setup user"
-    echo "a3. setup nfs firewalld"
-    echo "b......................( include config )"
-    echo "b1. setup nfs"
-    echo ""
-}
-
-# execute based on user choice
+# Execute based on user choice
 setup_execute_choice() {
     case "$1" in
         a)   a_xall;;
-        a1)  install_packages;;
-        a2)  all-ust;;
-        a3)  setup_firewalld;;
         b)   b_xall;;
-        b1)  nfs-setup;;
         *) echo "Invalid choice";;
     esac
 }
 
-# execute whole section
+# Execute whole section
 a_xall() {
-    all-ipa "$PMAN" "$PAK1" "$PAK2"
-    all-sdc "$SYSD_CHECK"
-    all-ust "$USERNAME1" "$PASSWORD1"
+    all_ipa "$PMAN" "$PAK1" "$PAK2"
+    all_sdc "$SYSD_CHECK"
+    all_ust "$USERNAME1" "$PASSWORD1"
 }
 
 b_xall() {
-    nfs-setup  "$NFS_HEADER_1" "$SHARED_FOLDER_1" "$NFS_OPTIONS_1"
-    nfs-setup  "$NFS_HEADER_2" "$SHARED_FOLDER_2" "$NFS_OPTIONS_2"
+    nfs_setup  "$NFS_HEADER_1" "$SHARED_FOLDER_1" "$NFS_OPTIONS_1"
+    nfs_setup  "$NFS_HEADER_2" "$SHARED_FOLDER_2" "$NFS_OPTIONS_2"
 }
