b7420cdd00c4991f7c6fdd7ec30c8d208edd2a9d
woeiru <169383590+woeiru@users.noreply.github.com>
2024-06-26 18:39:10 +0200

    set/.up: enhance debug output and input trimming

--- set/nfs.sh
+++ set/nfs.sh
@@ -18,12 +18,14 @@ setup_main "$DIR" "$FILE"
 
 # Execute whole section
 a_xall() {
+    echo "Executing a_xall"
     all_ipa "$PMAN" "$PAK1" "$PAK2"
     all_sdc "$SYSD_CHECK"
     all_ust "$USERNAME1" "$PASSWORD1"
 }
 
 b_xall() {
+    echo "Executing b_xall"
     nfs_setup  "$NFS_HEADER_1" "$SHARED_FOLDER_1" "$NFS_OPTIONS_1"
     nfs_setup  "$NFS_HEADER_2" "$SHARED_FOLDER_2" "$NFS_OPTIONS_2"
 }
