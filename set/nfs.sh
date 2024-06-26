9ab5734081820bb56253abe42321a440c556cc21
woeiru <169383590+woeiru@users.noreply.github.com>
2024-06-26 20:38:51 +0200

    standard

--- set/nfs.sh
+++ set/nfs.sh
@@ -13,14 +13,12 @@ MENU_OPTIONS[a]="a_xall"
 MENU_OPTIONS[b]="b_xall"
 
 a_xall() {
-    echo "Executing a_xall"
     all-ipa "$PMAN" "$PAK1" "$PAK2"
     all-sdc "$SYSD_CHECK"
     all-ust "$USERNAME1" "$PASSWORD1"
 }
 
 b_xall() {
-    echo "Executing b_xall"
     nfs-setup  "$NFS_HEADER_1" "$SHARED_FOLDER_1" "$NFS_OPTIONS_1"
     nfs-setup  "$NFS_HEADER_2" "$SHARED_FOLDER_2" "$NFS_OPTIONS_2"
 }
