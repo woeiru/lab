e4a96e05d87dc99ffcb2a33d288fadad9db3e1b5
woeiru <169383590+woeiru@users.noreply.github.com>
2024-07-07 19:10:53 +0200

    standard

--- fix/acpi/disable-devices-as-wakeup.sh
+++ fix/acpi/disable-devices-as-wakeup.sh
@@ -3,7 +3,7 @@
 LOGFILE="/var/log/disable-devices-as-wakeup.log"
 
 echo "Script started at $(date)" >> $LOGFILE
-declare -a devices=("GPP0") # <-- Add your entries here
+declare -a devices=("GPP0" "XHC1") # <-- Add your entries here
 for device in "${devices[@]}"; do
     if grep -qw ^$device /proc/acpi/wakeup; then
         status=$(grep -w ^$device /proc/acpi/wakeup | awk '{print $3}')
