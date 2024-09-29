2249a76054e23d18a00516e16393dd1b9cebdb82
woeiru <169383590+woeiru@users.noreply.github.com>
2024-09-29 18:05:26 +0200

    lib: create usr.bash by moving user-specific functions from all.bash and ini/user

--- lib/all.bash
+++ lib/all.bash
@@ -567,67 +567,6 @@ all-fec() {
   echo "$fstab_entry"
 }
 
-# Prompts the user to select a file from the current directory by displaying a numbered list of files and returning the chosen filename
-# variable select filename
-#
-all-vsf() {
-    files=($(ls))
-    echo "Select a file by entering its index:"
-    for i in "${!files[@]}"; do
-        echo "$i: ${files[$i]}"
-    done
-    read -p "Enter the index of the file you want: " index
-    sel="${files[$index]}"
-    echo "$selected_file"
-}
-
-# Counts files in directories based on specified visibility (regular, hidden, or both). Displays results sorted by directory name
-# count files folder
-# <path> <folder_type: 1=regular, 2=hidden, 3=both>
-all-cff() {
-    if [ $# -ne 2 ]; then
-	all-use
-        return 1
-    fi
-
-    local path="$1"
-    local folder_type="$2"
-    
-    # count files in a directory
-    count_files() {
-        local dir="$1"
-        find "$dir" -type f | wc -l
-    }
-    
-    # print directory information
-    print_directory_info() {
-        local dir="$1"
-        local file_count=$(count_files "$dir")
-        printf "%-20s %5s\n" "$dir" "$file_count"
-    }
-    
-    # Main function logic
-    case "$folder_type" in
-        1)
-            find "$path" -mindepth 1 -maxdepth 1 -type d -name '[^.]*' | while read -r dir; do
-                print_directory_info "$dir"
-            done
-            ;;
-        2)
-            find "$path" -mindepth 1 -maxdepth 1 -type d -name '.*' | while read -r dir; do
-                print_directory_info "$dir"
-            done
-            ;;
-        3)
-            find "$path" -mindepth 1 -maxdepth 1 -type d \( -name '[^.]*' -o -name '.*' \) | while read -r dir; do
-                print_directory_info "$dir"
-            done
-            ;;
-        *)
-            echo "Invalid folder type. Please provide either 1, 2, or 3."
-            ;;
-    esac | sort
-}
 
 # Creates and sends ZFS snapshots from a source pool to a destination pool. Supports initial full sends and incremental sends for efficiency
 # zfs dataset backup
@@ -690,73 +629,6 @@ all-zdb() {
     
     # Execute the send and receive commands
     eval "${send_cmd} | ${receive_cmd}"
-}
-
-# Compares data usage between two paths up to a specified depth. Displays results in a tabular format with color-coded differences
-# data usage comparison
-# <path1> <path2> <depth>
-all-duc() {
-    local path1=$1
-    local path2=$2
-    local depth=$3
-
-    if [ $# -ne 3 ]; then
-        all-use
-        return 1
-    fi
-
-    # Convert relative paths to absolute paths
-    path1=$(realpath "$path1")
-    path2=$(realpath "$path2")
-
-    # remove base path and sort by subpath
-    process_du() {
-        local path=$1
-        local depth=$2
-        du -b -d "$depth" "$path" | sed "s|$path/||" | sort -k2
-    }
-
-    # Process and sort du output for both paths
-    output1=$(process_du "$path1" "$depth")
-    output2=$(process_du "$path2" "$depth")
-
-    # Define ANSI color codes
-    RED='\033[0;31m'
-    NC='\033[0m'  # No Color
-
-    # Join the results on the common subpath
-    join -j 2 <(echo "$output1") <(echo "$output2") | awk -v p1="$path1" -v p2="$path2" -v red="$RED" -v nc="$NC" '
-        BEGIN {
-            OFS = "\t";
-            print "Path", p1, p2, "Difference"
-        }
-        function abs(value) {
-            return (value < 0) ? -value : value
-        }
-        function hr(bytes) {
-            if (bytes >= 1073741824) {
-                return sprintf("%.2fG", bytes / 1073741824)
-            } else if (bytes >= 1048576) {
-                return sprintf("%.2fM", bytes / 1048576)
-            } else if (bytes >= 1024) {
-                return sprintf("%.2fK", bytes / 1024)
-            } else {
-                return bytes "B"
-            }
-        }
-        {
-            subpath = $1
-            size1 = $2 + 0
-            size2 = $3 + 0
-            diff = abs(size1 - size2)
-            if (size1 < size2) {
-                print subpath, red hr(size1) nc, hr(size2), hr(diff)
-            } else if (size2 < size1) {
-                print subpath, hr(size1), red hr(size2) nc, hr(diff)
-            } else {
-                print subpath, hr(size1), hr(size2), hr(diff)
-            }
-        }' | column -t
 }
 
 
@@ -1173,31 +1045,7 @@ all-rsd() {
     esac
 }
 
-# Concatenates and displays the contents of all files within a specified folder, separating each file's content with a line of dashes
-# cat in folder
-# <path>
-all-cif() {
-    # Check if exactly one argument (directory path) is provided
-    if [ $# -ne 1 ]; then
-        all-use
-        return 1
-    fi
 
-    # Check if the argument is a directory
-    if [ ! -d "$1" ]; then
-        echo "$1 is not a directory."
-        return 1
-    fi
-
-    # Concatenate all files within the directory
-    for file in "$1"/*; do
-        if [ -f "$file" ]; then
-            echo "Contents of $file:"
-            cat "$file"
-            echo "-----------------------------------"
-        fi
-    done
-}
 
 # Adds a specified service to the firewalld configuration and reloads the firewall. Checks for the presence of firewall-cmd before proceeding 
 # firewall (add) service (and) reload
