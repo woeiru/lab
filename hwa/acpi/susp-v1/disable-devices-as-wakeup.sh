67e6fc0d8a93dbff32734995d4cb520867c437bb
woeiru <169383590+woeiru@users.noreply.github.com>
2024-10-18 20:20:59 +0200

    standard

--- hwa/acpi/susp-v1/disable-devices-as-wakeup.sh
+++ hwa/acpi/susp-v1/disable-devices-as-wakeup.sh
@@ -3,7 +3,18 @@
 LOGFILE="/var/log/disable-devices-as-wakeup.log"
 
 echo "Script started at $(date)" >> $LOGFILE
-declare -a devices=("GPP0" "GPP8" "XHC0") # <-- Add your entries here
+
+# Set USB power management to "on"
+echo "Setting USB power management to 'on'" >> $LOGFILE
+for usb_dev in /sys/bus/usb/devices/usb*/power/control; do
+    if [ -f "$usb_dev" ]; then
+        echo on > "$usb_dev"
+        echo "Set $usb_dev to 'on'" >> $LOGFILE
+    fi
+done
+
+# Disable specific ACPI wakeup devices
+declare -a devices=("GPP0") # <-- Add your entries here
 for device in "${devices[@]}"; do
     if grep -qw ^$device /proc/acpi/wakeup; then
         status=$(grep -w ^$device /proc/acpi/wakeup | awk '{print $3}')
@@ -18,4 +29,5 @@ for device in "${devices[@]}"; do
         echo "Device $device not found" >> $LOGFILE
     fi
 done
+
 echo "Script finished at $(date)" >> $LOGFILE
